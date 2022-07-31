a0cb290211a481857af0171917b4d07712cf144d
kursatcanciger <kursatcanciger@hotmail.com>
2022-07-31 17:25:13 +0300

    nested category system

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-
-  /**
-   * @Route("/", name="home")
-   */
-  public function index(): Response
+  #[Route('/', name: 'main')]
+  public function main()
   {
-    return $this->render('home_controller/index.html.twig');
+    return $this->redirectToRoute('home');
+  }
+
+  #[Route('/products/{slug}', name: 'home', requirements: ['slug' => '.*'])]
+  public function index(ManagerRegistry $doctrine, string $slug = null): Response
+  {
+    $category = null;
+
+    if ($slug) {
+      $categories = explode('/', $slug);
+      $categorySlug = array_pop($categories);
+
+      $category = $doctrine->getRepository(Category::class)->findBy(['slug' => $categorySlug]);
+    }
+
+    $categories = $doctrine->getRepository(Category::class)->findBy(['ParentCategory' => $category]);
+
+    return $this->render('home_controller/index.html.twig', ['categories' => $categories]);
   }
 }
